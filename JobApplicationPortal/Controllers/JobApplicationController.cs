b3e90bcbc97afa8b0b5556dc6c568f3cb96bf5b1
Arafat Zaman Anik <arafat.zaman.anik@gmail.com>
2024-10-14 05:45:52 +0600

    Add project files.

---
+++ JobApplicationPortal/Controllers/JobApplicationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JobApplicationPortal.Models;
+using System.IO;
+
+namespace JobApplicationPortal.Controllers
+{
+    public class JobApplicationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Apply(JobApplication application)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save resume file to wwwroot/resumes
+                if (application.Resume != null)
+                {
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/resumes", application.Resume.FileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        application.Resume.CopyTo(stream);
+                    }
+                }
+
+                // On success, redirect to Success page
+                return RedirectToAction("Success");
+            }
+
+            // If the model is invalid, return to form with validation errors
+            return View(application);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ JobApplicationPortal/Models/JobApplication.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationPortal.Models
+{
+    public class JobApplication
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Position applied for is required")]
+        public string Position { get; set; }
+
+        [Required(ErrorMessage = "Resume is required")]
+        [DataType(DataType.Upload)]
+        public IFormFile Resume { get; set; }
+    }
+}
